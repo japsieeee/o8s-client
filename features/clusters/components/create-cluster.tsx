a34f153494b31240c57637d6b7ec40f6a23db8a4
japs <dyollsopaj@gmail.com>
2025-10-16 14:08:08 +0900

    fix: function bug

--- features/clusters/components/create-cluster.tsx
+++ features/clusters/components/create-cluster.tsx
@@ -7,7 +7,7 @@ export default function CreateClusterButton() {
 
   return (
     <button
-      onClick={addCluster}
+      onClick={() => addCluster()}
       className='px-5 py-2.5 bg-indigo-600 text-white font-medium rounded-lg shadow hover:bg-indigo-700 transition-colors'
     >
       + Create Cluster
